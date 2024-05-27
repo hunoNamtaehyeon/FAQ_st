88cbea6068a3251a053fbce1d21cea085c6847cf
NamTaeHyeon <skaxogusdl@gmail.com>
2024-05-27 14:00:20 +0900

    asdg

--- FAQ_st.py
+++ FAQ_st.py
@@ -12,10 +12,12 @@ from openai import OpenAI
 
 st.set_page_config(layout="wide")
 
-df = pd.read_excel('./한난_23하 공채_FAQ_최종_230816_수정.xlsx', engine='openpyxl')
-df.dropna(axis=0, inplace=True)
-df = df[1:].reset_index(drop=True)
-df.columns = ['순번', '구분', '질문', '답변']
+with open('./df_to_json_with_vertor.json', 'r') as f:
+    df = json.load(f)
+j_df = pd.DataFrame(df)
+for_assistant_without_vector = copy.deepcopy(df)
+for row in for_assistant_without_vector:
+    del row['embedded_vector']
 ##############################################################################
 ##### local
 try:
@@ -33,7 +35,35 @@ def stream_data(m_c):
     for word in m_c.split(" "):
         yield word + " "
         time.sleep(0.05)
-        
+
+def cosine_similarity(a, b):
+    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
+def get_embedding(text, model):
+    return client.embeddings.create(input = [text], model=model).data[0].embedding
+
+def normalize_l2(x):
+    x = np.array(x)
+    if x.ndim == 1:
+        norm = np.linalg.norm(x)
+        if norm == 0:
+            return x
+        return x / norm
+    else:
+        norm = np.linalg.norm(x, 2, axis=1, keepdims=True)
+        return np.where(norm == 0, x, x / norm)
+    
+def search_docs(j_df, user_query):
+    embedding = get_embedding(
+        user_query,
+        model="text-embedding-3-small"
+    )
+    small_emb = normalize_l2(embedding[:512])
+    j_df["similarities"] = j_df['embedded_vector'].apply(lambda x: cosine_similarity(x, small_emb))
+    res = j_df.sort_values("similarities", ascending=False)
+    res = res.loc[res['similarities'] > 0.45][1:6]
+    return res
+       
 def get_current_weather(location, unit="fahrenheit"):
     """Get the current weather in a given location"""
     if "tokyo" in location.lower():
@@ -103,13 +133,14 @@ def run_conversation(messages, model):
     return response
 
 ###############################################################################################################
-
+    
 prompt = '''당신은 채용시스템의 FAQ기능을 대신합니다. 
             사용자의 질문에 올바른 답변을 합니다. 
             기준질문과 답변이 1:1로 매칭되어있는 아래 정보를 토대로 가장 맥락이 비슷한 답변을 내놓습니다.
             채용관련한 질문이 아니라면 "채용FAQ시스템입니다. 채용에 관련된 내용으로만 질문해주세요."라고 답변합니다.'''
             
-first_assistant = f'''기준질문-답변 데이터 : {df.to_json(orient='records',force_ascii=False)}
+
+first_assistant = f'''기준질문-답변 데이터 : {for_assistant_without_vector}
 
                        * 대화 예시 -> 
                        USER : 연봉 얼마야?
@@ -165,7 +196,7 @@ with col1:
         if user_input:
             st.session_state[f'messages_{radio}'].append({"role": "user", "content": user_input})
             
-    if user_input:        
+    if user_input:
         with chat_1:
             with st.chat_message('user'):
                 st.write(user_input)
@@ -176,11 +207,21 @@ with col1:
             st.session_state[f'messages_{radio}'].append({"role": answer_role, "content": answer_content})
             with st.chat_message(answer_role):
                 st.write_stream(stream_data(answer_content))
-                    
+                if "채용FAQ시스템입니다." not in answer_content:
+                    no1_question = answer_content.split('"')[1].split("]")[-1].strip()
+                    res = search_docs(j_df, no1_question)
+                    if len(res) > 0:
+                        with st.popover("유사질문 확인하기"):
+                            for rdx, row in res.iterrows():
+                                label = f"**[{rdx+1}] [{row['구분']}] {row['질문']}**"
+                                with st.expander(label, expanded=False):
+                                    st.divider()
+                                    st.write(row['답변'])
+
 with col2:
     st.header("FAQ - BORAD.ver")
     with st.container(height=700):
-        for rdx, row in df.iterrows():
+        for rdx, row in j_df.iterrows():
             label = f"**[{rdx+1}] [{row['구분']}] {row['질문']}**"
             with st.expander(label, expanded=False):
                 st.divider()
